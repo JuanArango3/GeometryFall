427563664d1855ec63ce05842ffdb0fbc7727fa3
JuanArango3 <jm.arango03@gmail.com>
2022-04-22 12:34:01 -0500

    Feat: Player sprite now extending AnimatedSprite

---
+++ Sprite/Player.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometryFall.Sprite
+{
+    class Player : AnimatedSprite
+    {
+        public Player(List<Texture2D> textures) :base(textures, new Point(300, 300), new Point(200, 200))
+        {
+
+        }
+    }
+}
