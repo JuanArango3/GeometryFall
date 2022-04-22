838557d375a47b2247cb608538acbe046e0df8b1
JuanArango3 <jm.arango03@gmail.com>
2022-04-22 12:33:07 -0500

    Added AnimatedSprite

---
+++ Sprite/AnimatedSprite.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometryFall.Sprite
+{
+    public class AnimatedSprite : SpriteBase
+    {
+        public List<Texture2D> Textures { get; private set; }
+        public int ActualTexture { get; set; }
+        public AnimatedSprite(List<Texture2D> textures, Rectangle r) : base(null, r)
+        {
+            Textures=textures;
+            ActualTexture = 0;
+        }
+        public AnimatedSprite(List<Texture2D> textures, Point size, Point location) : this(textures, new Rectangle(size, location))
+        {}
+
+        public new void Draw(SpriteBatch sb)
+        {
+            this.Texture = Textures[ActualTexture];
+            base.Draw(sb);
+        }
+    }
+}
